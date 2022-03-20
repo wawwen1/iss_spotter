44e0971ef00abfe2496eb81875d71d2376a58f0b
warren roque <warrenroque17@gmail.com>
2022-03-20 04:48:15 +0000

    promises insead of callback

---
+++ iss_promised.js
@@ -0,0 +1,27 @@
+const request = require('request-promise-native');
+
+const fetchIP = () => {
+  return request('https://api.ipify.org?format=json');
+};
+
+const fetchCoordsByIP = (body) => {
+  const ip = JSON.parse(body).ip;
+  return request(`https://freegeoip.app/json/${ip}`);
+};
+
+const fetchISSFlyOverTimes = (body) => {
+  const { latitude, longitude } = JSON.parse(body);
+  let url = `http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`;
+  return request(url);
+};
+
+const nextISSTimesForMyLocation = () => {
+  return fetchIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then(data => {
+      const { response } = JSON.parse(data);
+      return response;
+    })
+}
+module.exports = { nextISSTimesForMyLocation };
